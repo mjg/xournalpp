31fcbfd9f49d4b0c2a8c33929d399c8fe10a8b02
Michael J Gruber <github@grubix.eu>
2021-02-10 21:48:16 +0100

    Sidebar: provide stacked view
    Depending on the type of background and the way layers are used,
    previews of layers may make more sense either individually or as a
    stack.
    
    Provide an alternative sidebar preview "layer stack" which, for each
    layer, shows the layer and all layers below, including the background.
    
    Having this as a separate sidebar "page" (tab) make it easy to have
    one's favourite view there as well as to switch back and forth quickly.
    
    Technically, this sidebar view is so similar to the existing layer view
    that it is implemented as an option for the layer view rather than a new
    class.

--- src/control/jobs/PreviewJob.cpp
+++ src/control/jobs/PreviewJob.cpp
@@ -81,6 +81,7 @@ void PreviewJob::drawPage() {
                 break;  // out
             }           // else fall through for layer == -1
 
+        case RENDER_TYPE_PAGE_LAYERSTACK:
         case RENDER_TYPE_PAGE_PREVIEW:
             if (page->getBackgroundType().isPdfPage()) {
                 drawBackgroundPdf(doc);
@@ -105,6 +106,17 @@ void PreviewJob::drawPage() {
                 view.drawBackground();
             } else {
                 Layer* drawLayer = (*page->getLayers())[layer];
+                view.drawLayer(cr2, drawLayer);
+            }
+            view.finializeDrawing();
+            break;
+
+        case RENDER_TYPE_PAGE_LAYERSTACK:
+            // render all layers up to layer
+            view.initDrawing(page, cr2, true);
+            view.drawBackground();
+            for (int i = 0; i <= layer; i++) {
+                Layer* drawLayer = (*page->getLayers())[i];
                 view.drawLayer(cr2, drawLayer);
             }
             view.finializeDrawing();
